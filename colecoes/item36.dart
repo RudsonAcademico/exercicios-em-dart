f079794c21e61a3946d73c245e0c166a9061a6bb
Rúdson Alisson <silva.rudson@academico.ifpb.edu.br>
2026-05-21 22:45:37 +0000

    item37

---
+++ colecoes/item36.dart
@@ -0,0 +1,2 @@
+/* Converta a lista `` em um Set para remover duplicatas e exiba o
+resultado. */
---
+++ colecoes/item37.dart
@@ -0,0 +1,2 @@
+/* Crie um mapa var pessoa = {'nome': 'Ana', 'idade': 25}. Adicione
+a chave 'cidade' com valor 'São Paulo' e atualize 'idade' para 26. */
